a5fb8e31cebfc4e033337a7692737e13c728c2a7
Gaurav Kumar Yadav <156690582+Gaurav-Ydv@users.noreply.github.com>
2025-04-29 19:32:26 +0530

    Update RangeQuery.java

--- RangeQuery.java
+++ RangeQuery.java
@@ -8,13 +8,7 @@ public class RangeQuery {
         }
         return arr;
     }
-//    public static void printArray(int[] arr){
-//        for (int i = 0; i < arr.length; i++) {
-//            System.out.print(arr[i]+" ");
-//        }
-//        System.out.println();
-//
-//    }
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of the Array : ");
@@ -25,12 +19,8 @@ public class RangeQuery {
         for (int i = 1; i <= n ; i++) {
             arr[i] = sc.nextInt();
         }
-//        System.out.println("Original Array is ");
-//        printArray(arr);
 
         int[] prefixSum = makePrefixSumArray(arr);
-//        System.out.println("prefixSum is : ");
-//        printArray(arr);
 
         System.out.println("Enter number of queries ");
         int q = sc.nextInt();
